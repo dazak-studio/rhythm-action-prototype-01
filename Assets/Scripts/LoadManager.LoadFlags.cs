51235aad70d206e607b9fcf0bbbc191357fd1110
Carey Jo <casicos94@gmail.com>
2018-05-18 15:27:27 +0900

    Add the rhythm core from the original elgrado

---
+++ Assets/Scripts/LoadManager.LoadFlags.cs
@@ -0,0 +1,15 @@
+﻿public partial class LoadManager
+{
+    private enum LoadFlag
+    {
+        Bgm,
+       // Map,
+        
+        Count
+    }
+
+    private static int GetLoadFlagValue(LoadFlag loadFlag)
+    {
+        return 1 << (int)loadFlag;
+    }
+}
